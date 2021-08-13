11cfc107e2b16c2ce4bba5d581d16057e2eabfea
Luis Angel Barrancos Ortiz <luisbar@Luis-Angels-MacBook-Pro.local>
2021-08-13 16:48:01 -0400

    feat: create dropdown component

---
+++ src/components/atoms/Dropdown.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import { FormattedMessage } from 'react-intl'
+
+const Dropdown = ({ options }) => {
+
+  const renderOptions = () => {
+    return options.map(({key, value}) => {
+      return (
+        <FormattedMessage
+          id={value}
+          defaultMessage={value}
+        >
+          {
+            (value) => <option key={key} value={key}>{value}</option>
+          }
+        </FormattedMessage>
+      )
+    })
+  }
+
+  return (
+    <select>
+      {renderOptions()}
+    </select>
+  )
+}
+
+Dropdown.propTypes = {
+  options: propTypes.arrayOf(
+    propTypes.shape({
+      key: propTypes.string.isRequired,
+      value: propTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export default Dropdown
